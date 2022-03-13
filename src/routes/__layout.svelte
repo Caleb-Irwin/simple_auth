1134eb53283ff28691e4ecfe7babf8ca6907e461
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2022-03-12 23:21:15 -0600

    Basic background

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,1 +1,13 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+	import '../app.css';
+	import Background from '$lib/background.svelte';
+</script>
+
+<div id="content" class="w-full h-full">
+	<div class="w-full h-full grid place-items-center">
+		<div class="bg-white p-4 m-6 rounded-lg max-w-lg">
+			<slot />
+		</div>
+	</div>
+	<Background />
+</div>
