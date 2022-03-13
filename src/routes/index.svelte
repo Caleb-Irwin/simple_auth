c8564f091544ba0f4851358f1a887b588578d336
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2022-03-12 23:42:54 -0600

    new index, new files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl mb-2 text-center">Simple Auth</h1>
+<p>
+	A simple authenication service powered by <a
+		class="underline"
+		href="https://magic.link"
+		target="_blank"
+		rel="noopener noreferrer">magic.link</a
+	>
+</p>
+<div class="grid grid-cols-2 mt-2">
+	<a class="text-center text-lg m-1 bg-black p-1 text-white rounded-md" href="/login?cb=/callback"
+		>Demo</a
+	>
+	<a
+		class="text-center text-lg m-1 bg-black p-1 text-white rounded-md"
+		href="http://github.com/Caleb-Irwin/simple_auth/"
+		target="_blank"
+		rel="noopener noreferrer">Github</a
+	>
+</div>
