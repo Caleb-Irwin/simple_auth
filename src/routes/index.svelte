a0f04759b79e4764791867abd429d32bedc14ffe
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2022-03-13 18:05:36 -0600

    new styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,11 +8,12 @@
 	>
 </p>
 <div class="grid grid-cols-2 mt-2">
-	<a class="text-center text-lg m-1 bg-black p-1 text-white rounded-md" href="/login?cb=/callback"
-		>Demo</a
+	<a
+		class="text-center text-lg m-1 bg-black p-1 text-white rounded-md border-solid border-2 hover:border-black"
+		href="/login?cb=/callback">Demo</a
 	>
 	<a
-		class="text-center text-lg m-1 bg-black p-1 text-white rounded-md"
+		class="text-center text-lg m-1 bg-black p-1 text-white rounded-md border-solid border-2 hover:border-black"
 		href="http://github.com/Caleb-Irwin/simple_auth/"
 		target="_blank"
 		rel="noopener noreferrer">Github</a
